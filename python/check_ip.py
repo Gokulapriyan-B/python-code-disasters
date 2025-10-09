dda15188c2c0a80e8bc95c36d14bb51a1877af58
Sunandha Balaji <gbalaji@andrew.cmu.edu>
2025-10-08 22:38:56 -0400

    add random line in python/check_ip.py

--- python/check_ip.py
+++ python/check_ip.py
@@ -1,5 +1,5 @@
 from multiprocessing import Process
-
+print("Hello World")
 def split_list(alist, wanted_parts=1):
     length = len(alist)
     return [alist[i * length // wanted_parts: (i + 1) * length // wanted_parts] for i in range(wanted_parts)]
